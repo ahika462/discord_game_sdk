5e5084a87a83d023dd181eaa7fa04d7f9c9f5a42
h4master <107293097+h4master@users.noreply.github.com>
2024-02-12 19:18:49 +1100

    Update .gitignore

---
+++ examples/haxe/Main.hx
@@ -0,0 +1,39 @@
+import discord_game_sdk.Types;
+import haxe.Int64Helper;
+import discord_game_sdk.Core;
+
+class Main {
+	static function main() {
+		var clientID:String = '418559331265675294';
+		var core:Core = new Core();
+		Core.create(Int64Helper.parseString(clientID), NoRequireDiscord, core);
+		core.setLogHook(Debug, (level:LogLevel, message:String) -> trace('Log[$level] $message'));
+
+		var activity:Activity = {
+			details: 'Fruit Tarts',
+			state: 'Pop Snacks',
+			assets: {
+				smallImage: 'the',
+				smallText: 'i mage',
+				largeImage: 'the',
+				largeText: 'u mage'
+			},
+			secrets: {
+				join: 'join secret'
+			},
+			party: {
+				size: {
+					currentSize: 1,
+					maxSize: 5
+				},
+				id: 'party id',
+				privacy: Public
+			},
+			type: Playing
+		};
+		core.activityManager.updateActivity(activity, (r:Result) -> trace('${(r == Ok) ? 'Succeeded' : 'Failed'} updating activity!'));
+
+		while (true)
+			core.runCallbacks();
+	}
+}
